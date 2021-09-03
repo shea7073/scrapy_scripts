56a6480894dabd12e752fd43781b277f0015b7bf
shea7073 <shea7073@gmail.com>
2021-09-02 20:56:54 -0400

    steambot progress made

--- steambot/steambot/spiders/test2.py
+++ steambot/steambot/spiders/test2.py
@@ -13,7 +13,8 @@ class Test2Spider(scrapy.Spider):
                 'title': game.xpath(".//div[@class='responsive_search_name_combined']/div[1]/span/text()").get(),
                 'current_price': game.xpath(".//div[@class='col search_price_discount_combined responsive_secondrow']/@data-price-final").get(),
                 'discount': game.xpath(".//div[@class='col search_discount responsive_secondrow']/span/text()").get(),
-                'original_price': game.xpath(".//div[@class='responsive_search_name_combined']/div[4]/div[2]/span/strike/text()").get()
+                'original_price': game.xpath(".//div[@class='responsive_search_name_combined']/div[4]/div[2]/span/strike/text()").get(),
+                'release_date': game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
             }
         self.counter += 50
         if self.counter <= 300:
