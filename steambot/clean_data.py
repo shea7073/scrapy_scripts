e559aeb86a30d47260cb7288de7a40a0b9ebf3c9
shea7073 <shea7073@gmail.com>
2021-09-04 22:52:38 -0400

    steambot progress made 2

---
+++ steambot/clean_data.py
@@ -0,0 +1,30 @@
+import json
+import re
+
+def clean_data(file_name):
+
+    with open(file_name, encoding='utf-8') as f:
+        data = json.load(f)
+
+    de_duplicated = []
+    for dic in data:
+        if dic not in de_duplicated:
+            de_duplicated.append(dic)
+
+    for dic in de_duplicated:
+        dic['current_price'] = '$' + str(int(dic['current_price']) / 100)
+        num_pattern = "\d\d+%"
+        text_pattern = ""
+        if dic['rating'] and re.search(num_pattern, dic['rating']):
+            dic['num_score'] = re.search(num_pattern, dic['rating']).group(0)
+            dic['word_score'] =
+        else:
+            dic['num_score'] = None
+            dic['word_score'] = None
+
+
+        print(dic['num_score'])
+
+
+clean_data('test2.json')
+
--- steambot/steambot/spiders/test2.py
+++ steambot/steambot/spiders/test2.py
@@ -14,7 +14,8 @@ class Test2Spider(scrapy.Spider):
                 'current_price': game.xpath(".//div[@class='col search_price_discount_combined responsive_secondrow']/@data-price-final").get(),
                 'discount': game.xpath(".//div[@class='col search_discount responsive_secondrow']/span/text()").get(),
                 'original_price': game.xpath(".//div[@class='responsive_search_name_combined']/div[4]/div[2]/span/strike/text()").get(),
-                'release_date': game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get()
+                'release_date': game.xpath(".//div[@class='col search_released responsive_secondrow']/text()").get(),
+                'rating': game.xpath(".//span[contains(@class, 'search_review_summary')]/@data-tooltip-html").get()
             }
         self.counter += 50
         if self.counter <= 300:
